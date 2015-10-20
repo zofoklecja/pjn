333c36385a0eeb475d9b5b22d45a96985abfb226
Zofia Drozd <zegarynka@wp.pl>
2015-10-21 00:43:09 +0200

    fix split

--- diac_sets.py
+++ diac_sets.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python2
+#!/usr/bin/python
+
+#ą ę
+#
+#Napisz program diac_sets.py, który wypisze wszystkie takie zbiory wyrazów (jedna linia to jeden zbiór, wyrazy oddzielone spacją, kolejność nieistotna), w których wyrazy po usunięciu znaków diakrytycznych reprezentują to samo słowo (np. bąka, baka, baką). Program powinien czytać ze standardowego wejścia i pisać na standardowe wyjście. Przydatny może okazać się zestaw polskich liter diakrytyzowanych: 'ąćęłńóśźżĄĆĘŁŃÓŚŹŻ'.
 
 import sys
-import unicodedata
 
 for line in sys.stdin:
-    words = set(line.split(' ,.:!?'),"utf-8")
-    aski = set([unicodedata.normalize('NFD', unicode(word,"utf-8")).encode('ascii', 'ignore') for word in words])
-    for ask in aski:
-        for word in words:
-			if word==aski:
-				print "%s " % word
+    dic = {'ąćęłńóśźżĄĆĘŁŃÓŚŹŻ':'acelnoszzACELNOSZZ'}
+   
+    words = "".join((char if char.isalpha() else " ") for char in line).split()
+    words = set([word.lower() for word in words])
+    aski = set([word.translate(dic) for word in words])
 
-
+    print (words)
+    print (aski)
