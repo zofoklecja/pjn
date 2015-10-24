6eaec5b3c200f19ebc8ce5ffddd16fe0637dd331
Zofia Drozd <zegarynka@wp.pl>
2015-10-24 13:21:55 +0200

    Solve ą ę

--- diac_sets.py
+++ diac_sets.py
@@ -5,13 +5,19 @@
 #Napisz program diac_sets.py, który wypisze wszystkie takie zbiory wyrazów (jedna linia to jeden zbiór, wyrazy oddzielone spacją, kolejność nieistotna), w których wyrazy po usunięciu znaków diakrytycznych reprezentują to samo słowo (np. bąka, baka, baką). Program powinien czytać ze standardowego wejścia i pisać na standardowe wyjście. Przydatny może okazać się zestaw polskich liter diakrytyzowanych: 'ąćęłńóśźżĄĆĘŁŃÓŚŹŻ'.
 
 import sys
+import unicodedata
+
+def rmOgonki(text):
+    tab = str.maketrans("ąćęłńóśżźĄĆĘŁŃÓŚŻŹ", "acelnoszzACELNOSZZ")
+    return text.translate(tab)
 
 for line in sys.stdin:
-    dic = {'ąćęłńóśźżĄĆĘŁŃÓŚŹŻ':'acelnoszzACELNOSZZ'}
    
-    words = "".join((char if char.isalpha() else " ") for char in line).split()
-    words = set([word.lower() for word in words])
-    aski = set([word.translate(dic) for word in words])
-
-    print (words)
-    print (aski)
+    words = set("".join((char if char.isalpha() else " ") for char in line).split())
+    output = set([rmOgonki(word.lower()) for word in words])
+    
+    for out in output:
+        print("")
+        for word in words:
+            if rmOgonki(word.lower()) == out:
+                print(word, end=" ")  
