7d5d57310bead27fd7dc2bc5b0d2898cf4d0dc79
Zofia Drozd <zegarynka@wp.pl>
2015-10-27 00:31:32 +0100

    Polecenia na ćwiczenia

--- freq_words.py
+++ freq_words.py
@@ -1,3 +1,6 @@
+#Częste słowa
+# Napisz program freq_words.py który zlicza liczbę wystąpień słów w tekście. Program powinien czytać dowolny tekst ze standardowego wejścia i wypisywać na standardowym wyjściu listę składającą się z par 'liczba wystąpień' i 'słowo' oddzielone znakiem tabulacji. Lista wynikowa powinna być posortowana po liczbie wystąpień. Słowa należy normalizować względem wielkości liter. Które 10 słów najczęściej występuje w napisach filmowych (http://opus.lingfil.uu.se/download.php?f=OpenSubtitles%2Fmono%2FOpenSubtitles.pl.gz)?
+
 #!/usr/bin/python2
 
 import sys
